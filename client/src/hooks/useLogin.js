7d15174d5f00b7224bacf75e4d48a51534914ce7
criseria <criseria4212@naver.com>
2023-11-09 17:41:39 +0900

    수정본

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,21 @@
+import create from 'zustand';
+import axios from "axios";
+
+const useLogin = create((set) => ({
+  loading: false,
+  user: null,
+  error: null,
+  loginUser: async (body) => {
+    set({ loading: true });
+    try {
+      const response = await axios.post("http://localhost:8080/auth/login", body);
+      console.log('로그인 성공:', response);
+      set({ loading: false, user: response.data, error: null });
+    } catch (error) {
+      console.error('로그인 실패:', error);
+      set({ loading: false, user: null, error: error.response.data });
+    }
+  },
+}));
+
+export default useLogin;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,54 +1,55 @@
 import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { login } from '../redux/actions/authActions';
 import { useNavigate } from 'react-router';
+import useLogin from '../hooks/useLogin';
 
-function Login() {
-  // const dispatch = useDispatch();
-  const navigate = useNavigate();
+function LoginPage() {
+    const Login = useLogin();
+    const navigate = useNavigate();
 
-  const [userId, setUserid] = useState("");
-  const [Password, setPassword] = useState("");
+    const [userId, setUserid] = useState("");
+    const [Password, setPassword] = useState("");
 
-  const onIdHandler = (event) => {
-    setUserid(event.currentTarget.value);
-  }
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  }
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-
-    console.log('userId', userId);
-    console.log('Password', Password);
-
-    let body = {
-      userId: userId,
-      userPassword: Password,
+    const onIdHandler = (event) => {
+        setUserid(event.currentTarget.value);
     }
-    navigate('/');
-  }
 
-  return (
-    <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center',
-      width: '100%', height: '100vh'
-    }}>
-      로그인
-      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
-        <label>아이디</label>
-        <input type='text' value={userId} onChange={onIdHandler} />
-        <label>비밀번호</label>
-        <input type='password' value={Password} onChange={onPasswordHandler} />
-        <br />
-        <button type="submit">
-          Login
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        console.log('userId', userId);
+        console.log('Password', Password);
+
+        let body = {
+            userId: userId,
+            userPassword: Password,
+        }
+
+        Login.loginUser(body);
+
+        navigate('/');
+    }
+
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+                <label>아이디</label>
+                <input type='text' value={userId} onChange={onIdHandler} />
+                <label>비밀번호</label>
+                <input type='password' value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
                 </button>
-      </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
-export default Login;
+export default LoginPage;
