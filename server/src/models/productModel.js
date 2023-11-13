feacf72570c178ef42311fceab0c093e8c2574e1
Sungwoon95 <lad0305@naver.com>
2023-11-13 15:48:47 +0900

    feat: 프로덕트 스키마 생성

--- server/src/models/productModel.js
+++ server/src/models/productModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
   productName: { type: String, required: true },
-});
+  productDescription: { type: String, required: true },
+  productImage: { type: String, default: '' },
+  productPrice: { type: Number, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
+}, { timestamps: true, collection: 'products' });
 
 module.exports = mongoose.model("products", productSchema);
