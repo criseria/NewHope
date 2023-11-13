e5bede846bbf1d6f9b1436aca2b2ce09a7912688
Sungwoon95 <lad0305@naver.com>
2023-11-13 15:45:53 +0900

    feat:상품 상세 페이지 특정 상품 보여주기

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -1,22 +1,89 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router';
 import { fetcher } from '../utils/fetcher'
+import Text from '../components/text/Text';
+import Title from '../components/text/Title';
+import IrTitle from '../components/text/IrTitle';
+import useLogin from '../hooks/useLogin'
+import CenterImg from '../components/image/CenterImg'
+import Container from '../components/container/Container'
+import { Link } from 'react-router-dom'
+
 const ProductDetail = () => {
   const { id } = useParams()
+  const hasLogin = useLogin()
+  const navigate = useNavigate()
+  const [isNum, setIsNum] = useState(1)
   const [product, setProduct] = useState({})
   const getProduct = async () => {
     const res = await fetcher('get', `/product/${id}`)
-    // setProduct(res)
+    setProduct(res)
+  }
+
+  const onPlus = () => {
+    setIsNum(prev => prev + 1)
+  }
+  const onMinus = () => {
+    if (isNum <= 1) return
+    setIsNum(prev => prev - 1)
+  }
+
+  const onChangeHandle = (e) => {
+    setIsNum(parseInt(e.target.value.replace(/[^0-9]/g, '')))
+  }
+
+  const onPayments = async () => {
+    const payments = await fetcher('post', '/product/likes', { id, isNum })
   }
 
   useEffect(() => {
     getProduct()
   }, [id])
+  console.log(product)
   return (
-    <div>
-      {id}
-    </div>
+    <main>
+      <Link to={'/product'}>
+        Back
+      </Link>
+      {Object.keys(product).length !== 0 ?
+        <>
+          <IrTitle text={'상품 상세 페이지'} />
+          <div className='product__detail-product-wrap'>
+            <CenterImg src={product.productImage} alt={'상품 이미지'} />
+            <Container>
+              <Text text={product.owner.userName} />
+              {product.likes.length !== 0 ? <Text text={`${product.likes.length}명이 관심을 가지고 있어요`} /> : ""}
+              <Title text={product.productName} />
+              <Text text={product.productDescription} />
+              <Text cls={'product__detail-price'} text={`${product.productPrice.toLocaleString('ko-KR')}원`} />
+              <div className='product__detail-price-wrap'>
+                <div className='product__detail-amount-wrap'>
+                  <button onClick={onMinus}>-</button>
+                  <input type='text' value={isNum} readOnly />
+                  <button onClick={onPlus}>+</button>
+                </div>
+                <p>
+                  총 금액{(isNum * product.productPrice).toLocaleString('ko-KR')}원
+              </p>
+              </div>
+              <div className='product__detail-button-wrap'>
+                <div onClick={onPayments}>
+                  장바구니
+              </div>
+                <div onClick={onPayments}>
+                  관심등록
+              </div>
+                <div onClick={onPayments}>
+                  바로구매
+              </div>
+              </div>
+            </Container>
+          </div>
+        </>
+        : ''}
+    </main>
   )
 }
 
