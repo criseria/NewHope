5c89de599b2b8033781b83db6e1681912667c316
Sungwoon95 <lad0305@naver.com>
2023-11-13 09:37:43 +0900

    feat:IR 텍스트 컴포넌트 생성

---
+++ client/src/components/text/IrTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './text.css'
+
+const IrTitle = ({ text }) => {
+  return (
+    <h1 className='text__ir-text'>
+      {text}
+    </h1>
+  )
+}
+
+export default IrTitle
