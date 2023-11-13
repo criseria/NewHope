0b26d43cf095d6369cedabda0f596032d3d26cc3
Sungwoon95 <lad0305@naver.com>
2023-11-13 15:48:04 +0900

    feat: 상품 등록, 상품 리스트, 관심 상품 기능

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -1,15 +1,62 @@
-const productModel = require('../models/userModel')
+const productModel = require('../models/productModel')
+const userModel = require('../models/userModel')
 
 const product = async (req, res) => {
-  // const prodcutList = await productModel.find({})
-  res.send('send')
+  const productList = await productModel.find({})
+  res.send(productList)
+}
+
+const productCreate = async (req, res) => {
+  const { username, productName, productDescription, productImage, productPrice } = req.body
+  console.log(username, productName, productDescription, productImage, productPrice)
+  if (!productName || !productDescription || !productPrice) {
+    console.log('필수 영역이 입력되지 않았음')
+  }
+  const targetUser = await userModel.findById(username)
+  const newProduct = new productModel({ productName, owner: targetUser._id, productDescription, productImage, productPrice })
+  await newProduct.save()
+  console.log(newProduct)
 }
 
 const productDetail = async (req, res) => {
   const { id } = req.params
-  // const targetProdcut = await productModel.findById(id)
-  console.log(`${id}+end`)
-  res.send(`${id}+end`)
+  const targetProduct = await productModel.findById(id).populate('owner')
+  if (!targetProduct) return res.status(404).send('찾을 수 없는 페이지')
+  console.log(targetProduct)
+  res.send(targetProduct)
 }
 
-module.exports = { product, productDetail }
+const likes = async (req, res) => {
+  const { id, amount, username } = req.body
+  const targetUser = await userModel.findById(username)
+  const targetProduct = await productModel.findById(id)
+  if (!targetUser) return console.log('로그인이 필요합니다.')
+  const hasLiked = targetUser.likes.findIndex(i => i.equals(id))
+  const productLiked = targetUser.likes.findIndex(i => i.equals(username))
+
+
+  if (hasLiked >= 0) {
+    const newLiked = [...targetUser.likes]
+    const newUserLike = [...targetProduct.likes]
+    newLiked.splice(hasLiked, 1)
+    newUserLike.splice(productLiked, 1)
+    targetUser.likes = newLiked
+    targetProduct.likes = newUserLike
+
+    console.log(newLiked)
+    await targetUser.save()
+    await targetProduct.save()
+    return res.send(targetUser)
+  }
+  else {
+    targetUser.likes.push(id)
+    targetProduct.likes.push(id)
+    console.log(targetUser.likes)
+    await targetUser.save()
+    await targetProduct.save()
+    return res.send(targetUser)
+  }
+
+}
+
+module.exports = { product, productDetail, productCreate, likes }
