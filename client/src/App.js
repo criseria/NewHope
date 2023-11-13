9675964cc6b142b98b1fa533afc02dc6e4cd09ac
Sungwoon95 <lad0305@naver.com>
2023-11-13 09:21:43 +0900

    feat:상품 페이지,  상품 상세 페이지 생성

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login';
 import Register from './pages/Register'
 import NavBar from './components/NavBar';
+import Product from './pages/Product';
+import ProductDetail from './pages/ProductDetail';
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/product/:id' element={<ProductDetail />} />
         </Routes>
       </Router>
     </div >
