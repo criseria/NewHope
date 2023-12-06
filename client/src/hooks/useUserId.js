692a5cf782ff03dfbef864e7ba3cca43a093baf4
Sungwoon95 <lad0305@naver.com>
2023-12-06 11:03:39 +0900

    [styling]콘솔 로그 제거

--- client/src/hooks/useUserId.js
+++ client/src/hooks/useUserId.js
@@ -5,7 +5,6 @@ export const useUserId = create((set) => ({
   loading: null,
   getUserId: async () => {
     const res = await fetcher('get', '/product/get/oid')
-    console.log(res)
     set({ username: res })
   }
 }))
